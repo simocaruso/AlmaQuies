f755c6de9f74cd96c4b536fc3770ef5f98e7de31
simocaruso <simonecaruso998@gmail.com>
2025-11-27 21:07:25 +0100

    Camera follows player

--- src/engine/systems/camera_system.cpp
+++ src/engine/systems/camera_system.cpp
@@ -5,6 +5,9 @@
 #include "camera_system.hpp"
 
 #include "../components/camera_component.hpp"
+#include "../components/player_tag.hpp"
+#include "../components/transform_component.hpp"
+#include "../components/velocity_component.hpp"
 #include "events/zoom_command_event.hpp"
 
 CameraSystem::CameraSystem(entt::registry* registry, entt::dispatcher &dispatcher) : System(registry) {
@@ -23,4 +26,12 @@ void CameraSystem::on_zoom(ZoomCommandEvent zoom_event) {
 }
 
 void CameraSystem::update(int elapsed) {
+    auto camera = registry_->view<CameraComponent>().front();
+    const auto velocity = registry_->get<VelocityComponent>(camera);
+    if (!velocity.active) {
+        const auto player = registry_->view<PlayerTag>().front();
+        auto &camera_pos = registry_->get<TransformComponent>(camera).position;
+        const auto player_pos = registry_->get<TransformComponent>(player).position;
+        camera_pos = player_pos - Vec2{BUFF_W / 2.0f, BUFF_H / 2.0f};
+    }
 }
